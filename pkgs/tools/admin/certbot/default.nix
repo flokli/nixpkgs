f1d9ffa7c39abe3298bc07a87a1ee66fa63630a1
Félix Baylac-Jacqué <felix@alternativebit.fr>
2019-10-17 18:26:26 +0200

    certbot: 0.31.0 -> 0.39.0
    Updating:
    
    - nixos module to use the new `account_reg.json` file.
    - nixos tests to use acme-V02 protocol.
    - nixos tests TLS certificates + genscript according to new endpoint.
    
    Co-authored-by: Florian Klink <flokli@flokli.de>

--- pkgs/tools/admin/certbot/default.nix
+++ pkgs/tools/admin/certbot/default.nix
@@ -1,21 +1,44 @@
-{ stdenv, python3Packages, fetchFromGitHub, dialog }:
+{ stdenv, python37Packages, fetchFromGitHub, fetchurl, dialog, autoPatchelfHook, nginx }:
 
-python3Packages.buildPythonApplication rec {
+let
+  pebble_linux_amd64 = stdenv.mkDerivation rec {
+    name = "pebble";
+    version = "2.2.1";
+    src = fetchurl {
+      url = "https://github.com/letsencrypt/pebble/releases/download/v${version}/pebble_linux-amd64";
+      sha256 = "0frw0125px79sfqplaqx5dcfchwvm3il63p56qr2843j1amc1lxf";
+    };
+
+    nativeBuildInputs = [ autoPatchelfHook ];
+
+    phases = [ "installPhase" "fixupPhase" ];
+
+    installPhase = ''
+      mkdir -p $out/bin
+      install -m0755 ${src} $out/bin/pebble_linux_amd64
+    '';
+  };
+in python37Packages.buildPythonApplication rec {
   pname = "certbot";
-  version = "0.31.0";
+  version = "0.39.0";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "v${version}";
-    sha256 = "0rwjxmkpicyc9a5janvj1lfi430nq6ha94nyfgp11ds9fyydbh1s";
+    sha256 = "1s32xg2ljz7ci78wc8rqkjvgrz7vprb7fkznrlf9a4blm55pp54c";
   };
 
-  propagatedBuildInputs = with python3Packages; [
+  patches = [
+    ./0001-pebble_artifacts-hardcode-pebble-location.patch
+  ];
+
+  propagatedBuildInputs = with python37Packages; [
     ConfigArgParse
     acme
     configobj
     cryptography
+    distro
     josepy
     parsedatetime
     psutil
@@ -26,11 +49,19 @@ python3Packages.buildPythonApplication rec {
     zope_component
     zope_interface
   ];
-  buildInputs = [ dialog ] ++ (with python3Packages; [ mock gnureadline ]);
 
-  patchPhase = ''
+  buildInputs = [ dialog ] ++ (with python37Packages; [ mock gnureadline ]);
+
+  checkInputs = with python37Packages; [
+    pytest_xdist
+    pytest
+    dateutil
+  ];
+
+  postPatch = ''
     substituteInPlace certbot/notify.py --replace "/usr/sbin/sendmail" "/run/wrappers/bin/sendmail"
     substituteInPlace certbot/util.py --replace "sw_vers" "/usr/bin/sw_vers"
+    substituteInPlace certbot-ci/certbot_integration_tests/utils/pebble_artifacts.py --replace "@pebble@" "${pebble_linux_amd64}/bin/pebble_linux_amd64"
   '';
 
   postInstall = ''
@@ -40,7 +71,15 @@ python3Packages.buildPythonApplication rec {
     done
   '';
 
-  doCheck = !stdenv.isDarwin; # On Hydra Darwin tests fail with "Too many open files".
+  # tests currently time out, because they're trying to do network access
+  # Upstream issue: https://github.com/certbot/certbot/issues/7450
+  doCheck = false
+
+  checkPhase = ''
+    PATH="$out/bin:${nginx}/bin:$PATH" pytest certbot-ci/certbot_integration_tests
+  '';
+
+  dontUseSetuptoolsCheck = true;
 
   meta = with stdenv.lib; {
     homepage = src.meta.homepage;
