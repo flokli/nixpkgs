7f0a659e350c7cefaf7016fe5c5bc539890ae7ae
Florian Klink <flokli@flokli.de>
2019-10-31 23:37:18 +0100

    opensmtpd: 6.4.2p1 -> 6.6.0p1
    The substitition in smtpd/parse.y isn't necessary anymore (it's gone).
    
    opensmtpd 6.6.0p1 fails to build with libressl like this:
    
    ```
    make[1]: Entering directory '/build/opensmtpd-6.6.0p1'
    Making all in openbsd-compat
    make[2]: Entering directory '/build/opensmtpd-6.6.0p1/openbsd-compat'
    gcc -DHAVE_CONFIG_H -I. -I..  -I../smtpd -I../openbsd-compat -I../openbsd-compat/err_h -I/nix/store/7hgyarrzkhvp47ynwljabdfy28vzwjwb-libevent-2.1.11-dev/include   -ffunction-sections -c -o arc4random.o arc4random.c
    arc4random.c:167:21: error: macro "arc4random_stir" passed 1 arguments, but takes just 0
     arc4random_stir(void)
                         ^
    arc4random.c:168:1: error: expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
     {
     ^
    make[2]: *** [Makefile:445: arc4random.o] Error 1
    make[2]: Leaving directory '/build/opensmtpd-6.6.0p1/openbsd-compat'
    make[1]: *** [Makefile:419: all-recursive] Error 1
    make[1]: Leaving directory '/build/opensmtpd-6.6.0p1'
    make: *** [Makefile:351: all] Error 2
    ```
    
    so build with openssl for now.

--- pkgs/servers/mail/opensmtpd/default.nix
+++ pkgs/servers/mail/opensmtpd/default.nix
@@ -1,17 +1,17 @@
 { stdenv, fetchurl, autoconf, automake, libtool, bison
-, libasr, libevent, zlib, libressl, db, pam, nixosTests
+, libasr, libevent, zlib, openssl, db, pam, nixosTests
 }:
 
 stdenv.mkDerivation rec {
   pname = "opensmtpd";
-  version = "6.4.2p1";
+  version = "6.6.0p1";
 
   nativeBuildInputs = [ autoconf automake libtool bison ];
-  buildInputs = [ libasr libevent zlib libressl db pam ];
+  buildInputs = [ libasr libevent zlib openssl db pam ];
 
   src = fetchurl {
     url = "https://www.opensmtpd.org/archives/${pname}-${version}.tar.gz";
-    sha256 = "0pgv080ai7d98l9340jadp9wjiaqj2qvgpqhilcz0kps2mdiawbd";
+    sha256 = "1sgwbvc28h9nyyj4lv8d9b4ilzz03p2j1j763yr759k336a2193m";
   };
 
   patches = [
@@ -21,8 +21,6 @@ stdenv.mkDerivation rec {
   # See https://github.com/OpenSMTPD/OpenSMTPD/issues/885 for the `sh bootstrap`
   # requirement
   postPatch = ''
-    substituteInPlace smtpd/parse.y \
-      --replace "/usr/libexec/" "$out/libexec/opensmtpd/"
     substituteInPlace mk/smtpctl/Makefile.am --replace "chgrp" "true"
     substituteInPlace mk/smtpctl/Makefile.am --replace "chmod 2555" "chmod 0555"
     sh bootstrap
